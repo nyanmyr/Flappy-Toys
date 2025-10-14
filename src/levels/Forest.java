ecbe787e35220b0f5c18e9ff4dadd97540422c36
nyanmyr <202301638+nyanmyr@users.noreply.github.com>
2025-10-14 20:09:55 +0800

    (1.14) tiny update
    - created empty level classes
     • desert
     • forest
     • steamworks

---
+++ src/levels/Forest.java
@@ -0,0 +1,43 @@
+
+package levels;
+
+import levels.parallaxes.ParallaxLevel;
+
+public class Forest extends Level {
+
+    @Override
+    public void generateColumn(int gap, int columnOffset, int lifeTime) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void generateLeftGround(int offsetX) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void generateRightGround(int offsetX) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void generateBackground() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void generateLeftParallax(int offsetX, ParallaxLevel level) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void generateRightParallax(int offsetX, ParallaxLevel level) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void generateToken(int lifeTime) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
